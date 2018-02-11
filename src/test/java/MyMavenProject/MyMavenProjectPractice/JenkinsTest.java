88a8240e8435bfa847b2216ecfd2ebd1a9afea3b
RajendranSubramaniam <raju_testing@rediffmail.com>
2018-02-11 17:45:00 +0530

    new project

---
+++ src/test/java/MyMavenProject/MyMavenProjectPractice/JenkinsTest.java
@@ -0,0 +1,28 @@
+package MyMavenProject.MyMavenProjectPractice;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class JenkinsTest {
+
+    @Test
+    public void verifyTitle() throws Exception {
+	WebDriver driver = new FirefoxDriver();
+	driver.get("https://www.gmail.com");
+	driver.manage().window().maximize();
+	
+	Thread.sleep(4000);
+	
+	Dimension n = new Dimension(360,592);
+	driver.manage().window().setSize(n);
+	String pageTitle = driver.getTitle();
+	
+	Assert.assertEquals(pageTitle, "Gmail");
+	
+	
+	}
+	}
